bd7f798c5d08a3e1e49802195348a418ee01eb6e
TheCell <simon_hischier@hotmail.com>
2018-10-02 22:04:27 +0200

    added python for png input

---
+++ raspberryProgram/imageCreationFromPNG.py
@@ -0,0 +1,22 @@
+# convert a .png image file to a .bmp image file using PIL
+from PIL import Image
+from io import BytesIO
+import base64
+import re
+import epd7in5
+
+EPD_WIDTH = 640
+EPD_HEIGHT = 384
+
+def main():
+    epd = epd7in5.EPD()
+    epd.init()
+
+    fileIn = "fromServer.png"
+    img = Image.open(fileIn)
+
+    img.save("imageForEPaper.bmp", "BMP")
+    epd.display_frame(epd.get_frame_buffer(img))
+
+if __name__ == '__main__':
+    main()
